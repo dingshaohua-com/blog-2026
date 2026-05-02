b8da9d58f734d79427fc708d836aebb1f50ad003
丁少华 <960423114@qq.com>
2026-05-03 00:18:25 +0800

    fix: 集成api自动生成

---
+++ src/api/api.base.ts
@@ -0,0 +1,36 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
+
+axios.interceptors.response.use(
+    (response) => {
+      const res = response.data;
+      if (res.code === 1) {
+        // toast.error(res.msg);
+        alert(res.msg);
+        return Promise.reject(res.msg);
+      } else {
+        return res.data;
+      }
+    },
+    (error) => {
+     return Promise.reject(error);
+    },
+  );
+  
+  
+  // 类型工具：提取 API 响应中 data 字段的类型
+  type ExtractDataType<T> = T extends { data: infer D } ? D : T;
+  
+  export const customAxiosInstance = <T>(
+    config: AxiosRequestConfig,
+    options?: AxiosRequestConfig,
+  ): Promise<ExtractDataType<T>> => {
+    // 直接返回 axios 调用的结果
+    // 响应拦截器已经处理了数据解构，返回的就是最终的业务数据
+    config.baseURL='http://localhost:8080/api'
+    return axios({
+      ...config,
+      ...options,
+    }) as Promise<ExtractDataType<T>>;
+  };
+  
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,17 @@
 import { Link } from 'react-router';
 import HelloWorld from '@/components/hello-world';
+import { getAppInfo} from '@/api/endpoints/base';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(()=>{
+    getAppInfo().then(res=>{
+      console.log(res);
+      
+    })
+  },[])
+
   return (
     <div className="home">
       <Link to="/about"><button>about</button></Link>
